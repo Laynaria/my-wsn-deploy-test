31e5085ca7e6e6f728a98a0594f476ddd24a9037
laynaria <pro.laynaria@outlook.fr>
2024-04-17 11:05:17 +0200

    test for seeing real time logs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -48,7 +48,7 @@ function App() {
       <Footer>
         <Container>
           <p>&copy; 2020 Wild Code School</p>
-          <p>test modif staging automatic</p>
+          <p>test modif staging automatic + test logs</p>
         </Container>
       </Footer>
     </div>
